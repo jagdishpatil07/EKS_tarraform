4fedfc09b59823b0694adb06e13475522ba2a0d2
Jagdish Patil <130397851+jagdishpatil07@users.noreply.github.com>
2025-05-01 17:00:09 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "cluster_id" {
-  value = aws_eks_cluster.jagdish_eks.id
+  value = aws_eks_cluster.jagdish.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.jagdish_nodes.id
+  value = aws_eks_node_group.jagdish.id
 }
 
 output "vpc_id" {
